2158c19ccecf956e46d6b2b31066a23ed643b829
ChrisSong1994 <18557511748@163.com>
2023-03-06 21:42:14 +0800

    fix: import ace

--- src/renderer/components/Editor/JsonEditor.tsx
+++ src/renderer/components/Editor/JsonEditor.tsx
@@ -1,5 +1,6 @@
 import 'ace-builds/src-min-noconflict/ext-language_tools';
 import 'ace-builds/src-min-noconflict/ext-searchbox';
+import 'ace-builds/src-noconflict/ace';
 import 'ace-builds/src-noconflict/mode-json';
 import 'ace-builds/src-noconflict/theme-chrome';
 import AceEditor from 'react-ace';
