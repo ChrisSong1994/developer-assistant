d0ae6a482122f7632757e12c921fb1eada7215dc
chrissong <1352411860@qq.com>
2023-05-02 20:45:39 +0800

    feat: diff 语言切换

--- src/renderer/components/Editor/DiffEditor.tsx
+++ src/renderer/components/Editor/DiffEditor.tsx
@@ -1,27 +1,37 @@
 // @ts-ignore
 import AceDiff from 'ace-diff';
 import 'ace-diff/dist/ace-diff.min.css';
-import { useLayoutEffect, useRef } from 'react';
+import { useEffect, useLayoutEffect, useRef } from 'react';
 
 interface IProps {
   style?: Record<string, any>;
   options?: Record<string, any>;
+  language?: string;
 }
 
 const DiffEditor = (props: IProps) => {
+  const { language } = props;
   const editorRef = useRef<any>(null);
   const elementRef = useRef<any>(null);
 
+  useEffect(() => {
+    if (editorRef.current) {
+      editorRef.current.editors.left.ace.getSession().setMode(`ace/mode/${language}`);
+      editorRef.current.editors.right.ace.getSession().setMode(`ace/mode/${language}`);
+    }
+  }, [language]);
+
   useLayoutEffect(() => {
     editorRef.current = new AceDiff({
-      moge: 'ace/mode/json',
-      element: '#diff',
+      element: '#diff_editor',
       left: {
+        moge: 'ace/mode/json',
         editable: true,
         content: JSON.stringify({ a: 1 }, null, 2),
         copyLinkEnabled: false,
       },
       right: {
+        moge: 'ace/mode/json',
         editable: true,
         content: JSON.stringify({ a: 3 }, null, 2),
         copyLinkEnabled: false,
@@ -29,7 +39,13 @@ const DiffEditor = (props: IProps) => {
     });
   }, []);
 
-  return <div id="diff" ref={elementRef} style={{ width: '100%', height: '100%' }}></div>;
+  return (
+    <div
+      id="diff_editor"
+      ref={elementRef}
+      style={{ width: '100%', height: '100%', position: 'relative', border: '1px solid #e9e9e9' }}
+    ></div>
+  );
 };
 
 export default DiffEditor;
--- src/renderer/pages/Diff/index.tsx
+++ src/renderer/pages/Diff/index.tsx
@@ -1,18 +1,41 @@
-import { useMemo } from 'react';
+import { Select } from 'antd';
+import { useMemo, useState } from 'react';
 
 import { DiffEditor } from '@/components/Editor';
 import { useWindowSize } from '@/hooks';
 import styles from './index.less';
 
-const EDITOR_HEIGHT_PADDING = 180;
+const EDITOR_HEIGHT_PADDING = 130;
 
 const Diff = (props: any) => {
+  const [language, setLanguage] = useState<string>('text');
   const { height } = useWindowSize();
   const editorHeight = useMemo(() => height - EDITOR_HEIGHT_PADDING, [height]); // 编辑器高度
 
   return (
-    <div className={styles['diff']} style={{ height: editorHeight }}>
-      <DiffEditor />
+    <div className={styles['diff']}>
+      <div>
+        <span>语言：</span>
+        <Select
+          style={{ width: 180 }}
+          value={language}
+          onSelect={setLanguage}
+          options={[
+            {
+              value: 'json',
+              label: 'JSON',
+            },
+            {
+              value: 'text',
+              label: 'TEXT',
+            },
+          ]}
+        />
+      </div>
+
+      <div style={{ height: editorHeight }}>
+        <DiffEditor language={language} />
+      </div>
     </div>
   );
 };
