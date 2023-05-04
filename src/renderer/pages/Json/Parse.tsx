0cfff8f75bf85f7b50efbe1de902e071f8ca9293
ChrisSong1994 <18557511748@163.com>
2023-05-04 10:15:23 +0800

    chore: 处理文件名大小写问题

---
+++ src/renderer/pages/Json/Parse.tsx
@@ -0,0 +1,107 @@
+/**
+ * 1、json 格式化
+ * 2、json 解析
+ * 3、json 复制
+ * 4、显示行号（样式处理）
+ * 5、下载成json文件
+ * 6、支持 json5
+ * 7、支持转成json
+ */
+import Events from '@/utils/events';
+import { Tooltip } from 'antd';
+import jsonlint from 'jsonlint-mod';
+import { useEffect, useMemo, useState } from 'react';
+
+import Copy from '@/components/Copy';
+import { JsonEditor } from '@/components/Editor';
+import Icon from '@/components/Icon';
+import { useWindowSize } from '@/hooks';
+import { isEmpty } from '@/utils';
+import styles from './index.less';
+
+const EDITOR_HEIGHT_PADDING = 164;
+
+const JsonParseComponent = (props: any) => {
+  const [value, setValue] = useState('');
+  const [parseJson, setParseJson] = useState({});
+  const [parseError, setParseError] = useState(null);
+  const { height } = useWindowSize();
+  const editorHeight = useMemo(() => height - EDITOR_HEIGHT_PADDING, [height]); // 编辑器高度
+
+  // json 格式化
+  const handleJsonFormat = () => {
+    if (isEmpty(parseError)) {
+      setValue(JSON.stringify(parseJson, null, 2));
+    }
+  };
+
+  const handleCompress = () => {
+    if (isEmpty(parseError)) {
+      setValue(JSON.stringify(parseJson));
+    }
+  };
+
+  // 清除
+  const handleClear = () => {
+    setValue('');
+  };
+
+  // 保存
+  const handleSave = () => {
+    Events.saveFileToLocal({ fileName: 'test.json', payload: value });
+  };
+
+  // 导入文件
+  const handleImport = async () => {
+    const fileValue = await Events.getFileFromLocalPath({ filters: [{ name: 'json文件', extensions: ['*.json'] }] });
+    if (fileValue) setValue(fileValue);
+  };
+
+  // json 解析
+  const handleJsonParse = (value: string) => {
+    if (!isEmpty(value)) {
+      try {
+        const data = jsonlint.parse(value);
+        setParseJson(data);
+        setParseError(null);
+      } catch (err: any) {
+        setParseError(err.message);
+      }
+    } else {
+      setParseError(null);
+    }
+  };
+
+  useEffect(() => {
+    handleJsonParse(value);
+  }, [value]);
+
+  return (
+    <div className={styles['json-parse']}>
+      <div className={styles['json-panel']}>
+        <div className={styles['tool-panel']}>
+          <Copy value={value} size={18} />
+          <Tooltip placement="bottom" title="美化">
+            <Icon type="icon-qingchu" size={18} onClick={handleJsonFormat} />
+          </Tooltip>
+          <Tooltip placement="bottom" title="压缩">
+            <Icon type="icon-wenjianyasuo" size={18} onClick={handleCompress} />
+          </Tooltip>
+          <Tooltip placement="bottom" title="保存">
+            <Icon type="icon-baocun" size={18} onClick={handleSave} />
+          </Tooltip>
+          <Tooltip placement="bottom" title="导入">
+            <Icon type="icon-daoru" size={18} onClick={handleImport} />
+          </Tooltip>
+          <Tooltip placement="bottom" title="清除">
+            <Icon type="icon-shanchu" size={18} onClick={handleClear} />
+          </Tooltip>
+        </div>
+        <JsonEditor style={{ height: editorHeight }} value={value} onChange={setValue} />
+      </div>
+      {isEmpty(parseError) ? null : <div className={styles['error-panel']}>{parseError}</div>}
+    </div>
+  );
+};
+
+export default JsonParseComponent;
