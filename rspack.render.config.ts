23262cb93fd50342f5a219a9f68d071374f76057
宋军 <songjun@yugu.net.cn>
2024-11-25 22:27:02 +0800

    feat: build with rspack

---
+++ rspack.render.config.ts
@@ -0,0 +1,85 @@
+import { Configuration } from '@rspack/cli';
+import { rspack } from '@rspack/core';
+import RefreshPlugin from '@rspack/plugin-react-refresh';
+import path from 'path';
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const config: Configuration = {
+  entry: {
+    main: path.resolve(__dirname, './src/renderer/index.tsx'),
+  },
+  output: {
+    // publicPath: './',
+  },
+  resolve: {
+    extensions: ['...', '.ts', '.tsx', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src', 'renderer'),
+    },
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    historyApiFallback: false,
+  },
+  module: {
+    parser: {
+      'css/auto': {
+        namedExports: false,
+      },
+    },
+    rules: [
+      {
+        test: /\.(jsx?|tsx?)$/,
+        use: [
+          {
+            loader: 'builtin:swc-loader',
+            options: {
+              jsc: {
+                parser: {
+                  syntax: 'typescript',
+                  tsx: true,
+                },
+                transform: {
+                  react: {
+                    runtime: 'automatic',
+                  },
+                },
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.less$/,
+        loader: require.resolve('less-loader'),
+        options: {
+          lessOptions: {
+            javascriptEnabled: true,
+          },
+        },
+        type: 'css',
+      },
+      {
+        test: /\.(png|svg|webp|jpe?g|gif)(\?.*)?$/i,
+        type: 'asset',
+        generator: {
+          filename: 'assets/[hash][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new rspack.HtmlRspackPlugin({
+      template: path.resolve(__dirname, './src/renderer/index.html'),
+    }),
+    new rspack.ProgressPlugin({}),
+    isDev ? new RefreshPlugin() : null,
+  ].filter(Boolean),
+
+  experiments: {
+    css: true,
+  },
+};
+export = config;
