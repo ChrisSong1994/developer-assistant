a3ec6246b79d8f9cc5d6bc6edc65ca0a21fbc88e
ChrisSong1994 <18557511748@163.com>
2025-08-08 17:52:31 +0800

    fix: 正则速查表 url

--- src/renderer/pages/Regexp/CheatSheet.tsx
+++ src/renderer/pages/Regexp/CheatSheet.tsx
@@ -131,7 +131,7 @@ const EXAMPLE_DATASOURCE = [
   },
   {
     description: 'URL',
-    syntax: '^(https?://)?(www.)?[-a-zA-Z0-9@:%._+~#=]{2,256}.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)$',
+    syntax: '^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#()?&//=]*)$',
   },
   {
     description: 'HTML Tags',
