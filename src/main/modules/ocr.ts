b1a720e37c2c1b9307f24a365111f2d25b3f72cc
卷尺 <1352411860@qq.com>
2025-10-19 16:10:55 +0800

    chore: 屏蔽 ocr

--- src/main/modules/ocr.ts
+++ src/main/modules/ocr.ts
@@ -1,14 +1,17 @@
-import MacOCR from '@cherrystudio/mac-system-ocr';
-import path from 'path';
+// import MacOCR from '@cherrystudio/mac-system-ocr';
+// import path from 'path';
 
 // With options
 export async function getImgOcrText(imgPath: string) {
-  const options = {
-    languages: 'en-US, zh-Hans', // Specify recognition languages
-    recognitionLevel: MacOCR.RECOGNITION_LEVEL_ACCURATE,
-    minConfidence: 0.5,
-  };
+  // const options = {
+  //   languages: 'en-US, zh-Hans', // Specify recognition languages
+  //   recognitionLevel: MacOCR.RECOGNITION_LEVEL_ACCURATE,
+  //   minConfidence: 0.5,
+  // };
 
-  const text = await MacOCR.recognizeFromPath(path.resolve(imgPath), options);
-  return text;
+  // console.log('getImgOcrText', imgPath,options);
+  // const text = await MacOCR.recognizeFromPath(path.resolve(imgPath), options);
+
+  // console.log("text","text")
+  // return text;
 }
---
+++ src/renderer/pages/AstExplorer/index.tsx
@@ -0,0 +1,5 @@
+const AstExplorer = () => {
+  return <div className="ocr">ast</div>;
+};
+
+export default AstExplorer;
